6297e4f18eceddd899905371b4863e0edf7e7827
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-09-28 20:13:21 +0100

    Added HashMap example code and started Set

--- src/com/learn/maps/hashmap/HashMapExample.java
+++ src/com/learn/maps/hashmap/HashMapExample.java
@@ -1,34 +1,44 @@
 package com.learn.maps.hashmap;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapExample {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
+        //Initial Capacity: 16, Default Load Factor: 0.75
+        HashMap<String, String> hashMap = new HashMap<>();
 
-        print(map);
-        map.put("Tom", 10);
-        map.put("Matt", 30);
-        map.put("Pete", 20);
+        //Add Some Elements
+        hashMap.put("Processor", "Intel i7 5820K");
+        hashMap.put("Graphics Card", "GTX1080");
+        hashMap.put("Memory", "64GB");
 
-        System.out.println("Size of map is:- " + map.size());
+        //Retrieve Elements (Manually)
+        System.out.println("Printing out elements manually...\n");
+        String CPU = hashMap.get("Processor");
+        System.out.println("Processor: " + CPU);
+        String GPU = hashMap.get("Graphics Card");
+        System.out.println("Graphics Card: " + GPU);
+        String RAM = hashMap.get("Memory");
+        System.out.println("Memory: " + RAM);
 
-        print(map);
-        if (map.containsKey("Tom")) {
-            Integer a = map.get("Tom");
-            System.out.println("value for key \"Tom\" is:- " + a);
+        //Get EntrySet (A set of mapped entries)
+        Set<Map.Entry<String, String>> entrySet = hashMap.entrySet();
+        System.out.println("Printing out elements with an iterator...\n");
+        for (Map.Entry<String, String> entry : entrySet) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + ": " + value);
         }
 
-        map.clear();
-        print(map);
-    }
-
-    private static void print(Map<String, Integer> map) {
-        if (map.isEmpty()) {
-            System.out.println("map is empty");
-        } else {
-            System.out.println(map);
+        //Get KeySet (A set of keys from the HashMap)
+        Set<String> keySet = hashMap.keySet();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String key : keySet) {
+            stringBuilder.append(key).append(", ");
         }
+        System.out.println(stringBuilder.toString());
     }
 }
